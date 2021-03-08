deb125b42ee65213fe7123d6121b7e5ad7eb91a8
Aaron Reichman <areichman@mac.com>
2021-03-08 10:45:41 -0500

    Update button stories

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import { Button } from 'baseui/button';
+import styled from 'styled-components';
 
 export const Basic = (args) => (
   <Button {...args}>Click Me</Button>
 )
+
+export const Styled = () => {
+  const StyledButton = styled(Button)`
+    width: 100%;
+  `;
+  return <StyledButton>Styled Button</StyledButton>;
+}
+
+export const All = () => (
+  <React.Fragment>
+    <Button>Primary</Button>&nbsp;
+    <Button kind="secondary">Secondary</Button>&nbsp;
+    <Button kind="tertiary">Tertiary</Button>&nbsp;
+    <Button kind="minimal">Minimal</Button>&nbsp;
+  </React.Fragment>
+)
+
+All.parameters = {
+  controls: {
+    disabled: true,
+    hideNoControlsWarning: true,
+  },
+};
 
 const metadata = {
   title: 'Button',
   component: Button,
   argTypes: {
     kind: {
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'primary' },
+      },
       control: {
         type: 'inline-radio',
         options: ['primary', 'secondary', 'tertiary', 'minimal']
@@ -17,6 +45,10 @@ const metadata = {
       defaultValue: 'primary',
     },
     size: {
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'default' },
+      },
       control: {
         type: 'inline-radio',
         options: ['default', 'large', 'compact', 'mini'],
@@ -24,6 +56,10 @@ const metadata = {
       defaultValue: 'default',
     },
     disabled: {
+      table: {
+        type: { summary: 'bool' },
+        defaultValue: { summary: false },
+      },
       control: {
         type: 'inline-radio',
         options: [false, true]
--- src/themes/thorn-light.js
+++ src/themes/thorn-light.js
@@ -1,19 +1,34 @@
 import {createTheme} from 'baseui';
 
-// https://www.crispedge.com/color-shades-generator
+// thorn brand colors
+// shades generated via https://www.crispedge.com/color-shades-generator
+const coral = '#f87b5e';
+const indigo = '#221A45';
+const mint = '#66deca';
+
 const coral50 = '#fef0ed';
 const coral100 = '#fcd3c9';
 const coral200 = '#fbb5a5';
 const coral300 = '#f99881';
-const coral400 = '#f87b5e'; // thorn coral
+const coral400 = coral;
 const coral500 = '#dc6d53';
 const coral600 = '#c05f49';
 const coral700 = '#a5523e';
 
+const indigo50 = '#e6e5ea';
+const indigo100 = '#b5b2c1';
+const indigo200 = '#847f97';
+const indigo300 = '#534c6e';
+const indigo400 = indigo;
+const indigo500 = '#1e173d';
+const indigo600 = '#1a1435';
+const indigo700 = '#16112e';
+
+const mint600 = '#4fac9d';
+const mint700 = '#449486';
+
 const primitives = {
-  primary: coral400,
-  // primaryA: black,
-  // primaryB: white,
+  primary: coral,
   primary50: coral50,
   primary100: coral100,
   primary200: coral200,
@@ -22,6 +37,10 @@ const primitives = {
   primary500: coral500,
   primary600: coral600,
   primary700: coral700,
+  // primaryA: black,
+  // primaryB: white,
+
+  accent: indigo,
 };
 
 const overrides = {
@@ -29,6 +48,20 @@ const overrides = {
     buttonBorderRadius: '4px',
   },
   colors: {
+    // buttonPrimaryFill: coral,
+    // buttonPrimaryHover: coral700,
+    // buttonPrimaryActive: coral600,
+
+    buttonSecondaryText: 'white',
+    buttonSecondaryFill: indigo,
+    buttonSecondaryHover: indigo300,
+    buttonSecondaryActive: indigo200,
+
+    buttonTertiaryText: 'white',
+    buttonTertiaryFill: mint,
+    buttonTertiaryHover: mint700,
+    buttonTertiaryActive: mint600,
+
     borderSelected: coral400,
   }
 };
