32ec3a249af800474f290d45ff631b5148911597
Aaron Reichman <areichman@mac.com>
2021-05-07 13:09:16 -0400

    Update tab color

--- src/themes/ncmec-light/theme.js
+++ src/themes/ncmec-light/theme.js
@@ -2,20 +2,7 @@ import { createTheme } from 'baseui';
 import { colors } from './colors'
 
 const primitives = {
-  primary: colors.sun,
-  primaryA: colors.shipGray,
-  primaryB: 'white',
-
-  /*
-  primary50: colors.sun50,
-  primary100: colors.sun100,
-  primary200: colors.sun200,
-  primary300: colors.sun300,
-  primary400: colors.sun400,
-  primary500: colors.sun500,
-  primary600: colors.sun600,
-  primary700: colors.sun700,
-  */
+  primary: colors.shipGray,
 };
 
 const overrides = {
@@ -29,6 +16,7 @@ const overrides = {
     buttonPrimaryHover: colors.saffron,
     buttonSecondaryText: colors.blueLagoon,
     buttonTertiaryText: colors.blueLagoon,
+    borderSelected: colors.sun,
   },
 };
 
