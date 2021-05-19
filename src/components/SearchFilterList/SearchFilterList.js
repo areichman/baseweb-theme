d8d8f265692af50a3158b78ef517aa3be55a2375
Aaron Reichman <areichman@mac.com>
2021-05-19 08:46:14 -0400

    Use StyledPanel to pass overrides

--- src/components/SearchFilterList/SearchFilterList.js
+++ src/components/SearchFilterList/SearchFilterList.js
@@ -5,8 +5,8 @@ import { RadioGroup, Radio } from 'baseui/radio'
 const PanelOverrides = {
   Content: {
     style: {
-      // backgroundColor: 'transparent',
-      paddingTop: '10px',
+      backgroundColor: 'transparent',
+      paddingTop: '0px',
       paddingBottom: '10px',
       paddingLeft: '20px',
       paddingRight: '20px',
@@ -14,28 +14,32 @@ const PanelOverrides = {
   }
 }
 
+const StyledPanel = (props) => (
+  <Panel {...props} overrides={PanelOverrides} />
+)
+
 function SearchFilterList() {
   return (
     <Accordion accordion={false}>
-      <Panel title="Date" overrides={PanelOverrides}>
+      <StyledPanel title="Date">
         <RadioGroup>
           <Radio value="2021-01-01">2021-01-01</Radio>
           <Radio value="2021-01-08">2021-01-08</Radio>
         </RadioGroup>
-      </Panel>
+      </StyledPanel>
 
-      <Panel title="Status" overrides={PanelOverrides}>
+      <StyledPanel title="Status">
         <Checkbox>First Pass</Checkbox>
         <Checkbox>Suggested</Checkbox>
         <Checkbox>Matched</Checkbox>
         <Checkbox>Conflict</Checkbox>
-      </Panel>
+      </StyledPanel>
 
-      <Panel title="ESP" overrides={PanelOverrides}>
+      <StyledPanel title="ESP">
         <Checkbox>Google</Checkbox>
         <Checkbox>Facebook</Checkbox>
         <Checkbox>Instagram</Checkbox>
-      </Panel>
+      </StyledPanel>
     </Accordion>
   )
 }
